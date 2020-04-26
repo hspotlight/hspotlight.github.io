48655cc188951feaa92214f1b191e3013a92c8fe
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-26 14:20:57 +0700

    URe-build github page using React

---
+++ precache-manifest.d74dc70524140fe130afcc2b5010a930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea361c7f2e4c3f5691e22863094f1b4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd4cf8e586e5ec4e9ea",
+    "url": "/static/css/main.31c086a0.chunk.css"
+  },
+  {
+    "revision": "028cb8335b1b97b1a348",
+    "url": "/static/js/2.571e4c00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.571e4c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd4cf8e586e5ec4e9ea",
+    "url": "/static/js/main.2d302b9e.chunk.js"
+  },
+  {
+    "revision": "4794136812554d8b55ca",
+    "url": "/static/js/runtime-main.d72a31e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
